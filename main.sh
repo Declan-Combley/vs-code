ffb59b073bf5c17c7343b9459652c01d0d532968
Declan-Combley <declan-combley@protonmail.com>
2022-04-10 16:58:45 +0800

    changing settings.json functionality

--- main.sh
+++ main.sh
@@ -13,3 +13,12 @@ echo "installed all extensions..."
 echo "themes you may like: "
 echo "Material Icon Theme"
 echo "GlassIt-VSC"
+
+cd 
+
+echo "updating settings.json..."
+
+> /home/declan/.config/Code - OSS/User 
+cat settings.json > /home/declan/.config/Code - OSS/User 
+
+echo "done ..."
