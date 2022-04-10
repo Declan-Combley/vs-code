221e8cf2b030e2834cb5972892b1e5ca4f2241fd
Declan-Combley <declan-combley@protonmail.com>
2022-04-10 16:01:48 +0800

    Installs vscode and assocciated extenstions

---
+++ main.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo pacman -Syy && sudo pacman -S code && echo "installation done ..."
+
+code --install-extension Tabnine.tabnine-vscode  
+code --install-extension ritwickdey.liveserver 
+code --instal-extension markwylde.vscode-filesize
+code --instal-extension eamodio.gitlens
+code --instal-extension vscodevim.vim
+
+echo "installed all extensions..."
+
+echo "themes you may like: "
+echo "Material Icon Theme"
+echo "GlassIt-VSC"
